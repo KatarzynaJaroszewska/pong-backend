55a9188b50c934bb1e40276a6c80ec58719b4eb4
kassia183 <katarzyna_jaroszewska@wp.eu>
2023-01-15 08:54:45 +0000

    Update 3 files

--- src/gateway/pong.gateway.ts
+++ src/gateway/pong.gateway.ts
@@ -7,6 +7,7 @@ import {
 } from "@nestjs/websockets/decorators";
 import { OnModuleInit } from "@nestjs/common";
 import { Server, Socket } from "socket.io";
+import { Pong } from "./types";
 
 @WebSocketGateway({
   cors: {
@@ -15,20 +16,42 @@ import { Server, Socket } from "socket.io";
 })
 export class PongGateway implements OnModuleInit {
   players: { id: string; name: string }[] = [];
+  pong: Pong;
 
   @WebSocketServer()
   server: Server;
 
   onModuleInit() {
     this.server.on("connection", (socket: Socket) => {
-      //   this.players = this.players.filter((p) => p.id !== socket.id);
       this.server.emit("pong.members", this.players);
+
+      socket.on("disconnecting", () => {
+        this.players = this.players.filter((p) => p.id !== socket.id);
+        this.server.emit("pong.members", this.players);
+        if (this.players.length < 2) {
+          this.pong = null;
+        }
+      });
     });
+
+    setInterval(() => {
+      if (this.players.length >= 2 && !this.pong) {
+        this.pong = new Pong(600, 400, this.players[0], this.players[1]);
+      }
+      if (this.pong) {
+        this.pong.update();
+        this.server.emit("pong.cast", this.pong.show());
+      }
+    }, 15);
   }
 
   @SubscribeMessage("chat-message")
   chat(@ConnectedSocket() socket: Socket, @MessageBody() data: any) {
-    socket.broadcast.emit("chat-message", data);
+    const name = this.players.find((p) => p.id === socket.id)?.name;
+    socket.broadcast.emit("chat-message", {
+      message: data,
+      name: data.name ?? "Annonymous",
+    });
     console.log(`New message from ${socket.id}`, data);
   }
 
@@ -39,5 +62,27 @@ export class PongGateway implements OnModuleInit {
   ) {
     this.players.push({ id: socket.id, name });
     this.server.emit("pong.members", this.players);
+  }
+
+  @SubscribeMessage("pong.move")
+  pongMove(@ConnectedSocket() socket: Socket, @MessageBody() key: string) {
+    const player = this.pong && this.pong.player(socket.id);
+    if (!player) {
+      return;
+    }
+    switch (key) {
+      case "ArrowUp": {
+        player.move(-10);
+        break;
+      }
+      case "ArrowDown": {
+        player.move(10);
+        break;
+      }
+      default: {
+        player.move(0);
+        break;
+      }
+    }
   }
 }
